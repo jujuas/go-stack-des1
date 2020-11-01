d7863a6d7b62572b0c73ac32c7267a797e54d4d3
jujuas <34321418+jujuas@users.noreply.github.com>
2020-10-31 21:36:36 -0300

    Primeira alteracao

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+//const {uuid} = require('uuidv4');
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+ const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -10,24 +11,54 @@ app.use(cors());
 
 const repositories = [];
 
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  const {title} = request.query;
+  const results = title ? repositories.filter(repository => repository.title.includes(title)) : repositories;
+  return response.json(results);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title,url,techs} = request.body;
+  const repository = {id:uuid(),title:title,url:url,techs:techs,likes:0};
+
+  repositories.push(repository);
+  return response.json(repositories);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id}=request.params;
+  const repositoryId = repositories.findIndex(repository => repository.id == id);
+  if(repositoryId<0){
+    return response.status(400).json({error:'Repository does not exists'});
+  }
+  const {title,url,techs} = request.body;
+  const repository = {
+    id,title,url,techs
+  };
+  repositories[repositoryId] = repository;
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const repositoryId = repositories.findIndex(repository => repository.id == id);
+  if (repositoryId<0){
+    return response.status(400).json({error:'Repository does not exists'});
+  }
+  repositories.splice(repositoryId,1);
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const repositoryId = repositories.findIndex(repository=>repository.id == id);
+  if (repositoryId<0){
+    return response.status(400).json({error:'Repository does not exists'});
+  }
+  repositories[repositoryId].likes += 1;
+  return response.json(repositories[repositoryId]);
+  
 });
 
 module.exports = app;
